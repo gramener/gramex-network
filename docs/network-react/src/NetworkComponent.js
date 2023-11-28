d65fa54a4388f8675e734d4375c9885c17aad647
S Anand <root.node@gmail.com>
2023-11-28 08:05:11 +0800

    FIX: React component uses useRef

--- docs/network-react/src/NetworkComponent.js
+++ docs/network-react/src/NetworkComponent.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 import { network } from "@gramex/network";
 
 const NetworkComponent = () => {
   const [data, setData] = useState({});
+  const documapRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,16 +15,16 @@ const NetworkComponent = () => {
   }, []);
 
   useEffect(() => {
-    if (data.name) {
+    if (data.name && documapRef.current) {
       var root = d3.hierarchy(data);
-      const graph = network("#network", { links: root.links(), nodes: root.descendants(), d3 });
+      const graph = network(documapRef.current, { links: root.links(), nodes: root.descendants(), d3 });
       const color = d3.scaleOrdinal(d3.schemeCategory10);
       graph.nodes.attr("fill", (d) => color(d.depth)).attr("r", (d) => 10 - d.depth * 2);
       graph.links.attr("stroke", "rgba(0,0,0,0.2)");
     }
   }, [data]);
 
-  return <svg id="network" width="600" height="380"></svg>;
+  return <svg ref={documapRef} width="600" height="380"></svg>;
 };
 
 export default NetworkComponent;
